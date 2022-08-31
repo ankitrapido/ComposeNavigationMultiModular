9bba32c0e0b3749e907f9906b399351b2c277a81
Ankit <ankit.k@rapido.bike>
2022-08-31 13:27:41 +0530

    raamcosta lib added for navigation

---
+++ features/listings/src/main/java/pseudoankit/droid/listings/ListingComposable.kt
@@ -0,0 +1,21 @@
+package pseudoankit.droid.listings
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@Destination
+@Composable
+fun ShowListings(
+    navigator: DestinationsNavigator
+) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        Text(text = "I will show list items")
+    }
+}
