594def615e217976a5642d6f0c9bb9ab80ad3593
Ankit <ankit.k@rapido.bike>
2022-09-01 19:03:43 +0530

    modules added in buildSrc Modules.kt file

--- buildSrc/src/main/java/Modules.kt
+++ buildSrc/src/main/java/Modules.kt
@@ -1,4 +1,8 @@
 object Modules {
     const val app = ":app"
-
+    const val listings = ":features:listings"
+    const val detail = ":features:detail"
+    const val home = ":features:home"
+    const val onboarding = ":features:onboarding"
+    const val navigation = ":core:navigation"
 }
